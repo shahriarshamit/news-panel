44c24ccd712427416e25c18f65d56569ef86cadd
Shahriar <shahriar.shamit@gmail.com>
2022-11-02 00:51:35 +0600

    Initial commit

---
+++ app/Controllers/Pages.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controllers;
+
+class Pages extends BaseController {
+
+    public function index() {
+        return view('welcome_message');
+    }
+
+    public function view($page = 'home') {
+        if (!is_file(APPPATH . 'Views/pages/' . $page . '.php')) {
+            throw new \CodeIgniter\Exceptions\PageNotFoundException($page);
+        }
+
+        $dateYear = new \DateTime();
+        $data['title'] = ucfirst($page);
+        $data['year'] = $dateYear->format('Y');
+
+        return view('templates/header', $data)
+                . view('pages/' . $page)
+                . view('templates/footer', $data);
+    }
+
+}
---
+++ app/Views/templates/footer.php
@@ -0,0 +1,3 @@
+        <em>&copy; <?= esc($year); ?></em>
+    </body>
+</html>
